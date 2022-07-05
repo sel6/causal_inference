7c3d3056635f57abcbee43037192aabc066721b5
Selam <84750917+sel6@users.noreply.github.com>
2022-07-05 13:15:23 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,18 @@
 
+import os
+import sys
+import unittest
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../scripts')))
+
+class TestFiles(unittest.TestCase):
+
+  def test_read_csv(self):
+    df = US.read_csv('test.csv')
+    df1 = pd.read_csv('test.csv')
+    self.assertEqual(df.shape, df1.shape)
+    
+    
+if __name__ == '__main__':
+  unittest.main()
